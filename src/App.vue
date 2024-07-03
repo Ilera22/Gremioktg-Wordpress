5e62af26c860b6acc2ed7f66d4d6d25b3b787713
Ilera22 <ilera.none@gmail.com>
2024-07-03 11:04:59 -0500

    src

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <nav><NavBar /></nav>
+    
+    <v-main>
+      <div style="height: 2%;"></div>
+      <TestComponent />
+      <router-view />
+      <TestComponent />
+    </v-main>
+
+    <footer>
+      <AppFooter />
+    </footer>
+  </v-app>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import AppFooter from './components/AppFooter.vue'
+import TestComponent from './components/TestComponent.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    AppFooter,
+    TestComponent,
+  },
+};
+</script>
+
+<style>
+/* Aquí puedes agregar estilos globales o específicos para tu App.vue */
+</style>
